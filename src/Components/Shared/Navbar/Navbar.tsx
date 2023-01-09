e743c43696e161b939b312127ce827bf3ad46736
netotorres1 <torresneto47@gmail.com>
2023-01-09 18:15:17 -0300

    home mobile finalizada

--- src/Components/Shared/Navbar/Navbar.tsx
+++ src/Components/Shared/Navbar/Navbar.tsx
@@ -1,21 +1,35 @@
 import React from 'react'
+import { useState } from 'react';
+import {MdMenu, MdClose} from 'react-icons/md'
+
 
 import './navbar.sass'
 
 const logo = require('./../../../assets/logo.svg').default;
+const bg = require('./../../../assets/bg-pattern-about-1-mobile-nav-1.svg').default;
 
 function Navbar() {
+
+  const [openMenu, setOpenMenu] = useState(false) 
+
+  const handleOpen = () => {
+    setOpenMenu(!openMenu)
+
+  }
+
   return (
     <header className='container-header'>
         <div className='container-content'>
-            <div>
-                <img src={logo} />
+          <img className='logo' src={logo} />
+            <div className={openMenu === false ? 'navbar' : 'open'}>
                 <nav>
                     <button>home</button>
                     <button>about</button>
                 </nav>
+                <button className='btncontact'>contact us</button>
+                <img className='bgnav' src={bg} />
             </div>
-            <button className='btncontact'>contact us</button>
+            <button onClick={handleOpen} className='btnMenu'>{openMenu === false ? <MdMenu size={40} color={'#fff'}/> : <MdClose size={40} color={'#fff'}/>}</button>
         </div>
     </header>
   )
